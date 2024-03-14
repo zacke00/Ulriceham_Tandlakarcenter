d87b42ff4a8e78a68dfea07f115d72cd5d611162
Zakaria Berglund <zacke.berglund@outlook.com>
2024-03-14 14:54:11 +0100

    Changed the header and the design on the main page

--- website/src/Components/Header/Index.tsx
+++ website/src/Components/Header/Index.tsx
@@ -1,32 +1,33 @@
-import React, { SVGProps } from "react"
+import React, { SVGProps, useEffect, useState } from "react"
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom"
 import {AboutUsPage} from "../../Pages/About-us/Index"
 import {ContactPage}  from "../../Pages/Contact/Index"
 import {HomePage}  from "../../Pages/Home/Index"
-import "../../Style.css"
+import "./Header.css"
 import { HomeIcon,AboutIcon, ContactIcon } from "../IconHandler/Index"
 
-export const Header = () => {
+export const Header: React.FC = () => {
+
     return (
-        <div id="Header-Container">
+        <div id="Header-Container" >
             <Router>
                 <div id="Header-Inner-Container">
                     <div id="Header-Icon"> THIS IS MADE FOR ICON</div>
                     <div id="Header-Pages-Container">
                         
                             <Link id="Header-Home-Link" to="/">
-                                <HomeIcon style={{height: "20px", fill: "#194569"}}/>
+
                                 Hem
                             </Link>
                        
                             <Link id="Header-About-Link" to="/about-us">
-                                <AboutIcon style={{height: "20px", fill: "#194569"}}/>
+
                                 
                                 Om oss
                             </Link>
                        
                             <Link id="Header-Contact-Link" to="/contact">
-                                <ContactIcon style={{ height: "20px", fill: "#194569"}}/>
+
                                 Kontakt
                             </Link>
                         
@@ -43,6 +44,7 @@ export const Header = () => {
                 </Routes>
             </Router>
         </div>
+
     )
 }
 
--- website/src/Pages/Home/Index.tsx
+++ website/src/Pages/Home/Index.tsx
@@ -1,11 +1,14 @@
-import React from "react";
-
+import React, { useState } from "react";
+import "./Home.css"
 
 export const HomePage = () => {
+  const [hoveredItem, setHoveredItem] = useState(null as string | null);
 
     return (
-    <div style={{ position: 'relative', height: '70vh', overflow: 'hidden' }}>
-    <video 
+    <div id="Home-Container">
+
+      <div id="Home-Inner-Container" style={{ position: 'relative', height: '70vh', overflow: 'hidden', paddingTop: 100}}>
+      <video 
       style={{
         position: 'absolute',
         width: '100%',
@@ -21,13 +24,47 @@ export const HomePage = () => {
       loop
       muted
       playsInline // Helps with autoplay on some browsers/mobile devices
-    >
+      >
     <source src="/Videos/Dental-Care.mp4" type="video/mp4" />
-     
       Your browser does not support the video tag.
     </video>
-    <h1 style={{ zIndex: '1', position: 'relative', textAlign: 'center', top: '20%' }}>Home</h1>
-  </div>
+        <div id="Home-Title">
+          <h1>Home</h1>
+        </div>
+      </div>
+
+      <div id="Home-Main-Section-Container">
+      <div id="Home-Section-Container">
+        <div id="Home-First-Item"
+          onMouseEnter={() => setHoveredItem('first')}
+          onMouseLeave={() => setHoveredItem(null)}
+          style={{ opacity: hoveredItem && hoveredItem !== 'first' ? 0.5 : 1 }}
+        >
+          <h3>First Container</h3>
+        </div>
+        <div id="Home-Second-Item" 
+          onMouseEnter={() => setHoveredItem('second')}
+          onMouseLeave={() => setHoveredItem(null)}
+          style={{ opacity: hoveredItem && hoveredItem !== 'second' ? 0.5 : 1 }}
+        >
+          <h3>Second Container</h3>
+        </div>
+        <div id="Home-Third-Item"
+          onMouseEnter={() => setHoveredItem('third')}
+          onMouseLeave={() => setHoveredItem(null)}
+          style={{ opacity: hoveredItem && hoveredItem !== 'third' ? 0.5 : 1}}>
+          <h3>Third Container</h3>
+        </div>
+
+        
+      </div>
+      </div>
+      <div id="Home-Information-Container">
+        <h2>Information</h2>
+        <div style={{marginBottom: "300rem"}}></div>
+      </div>
+    </div>
     )
 
 }
+
